8e1b1027b0a4bf0361f24e3c8877c8e77d3ba710
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2026-07-19 02:30:22 +0300

    4: add logging to api project

--- HomeLabCore.Api/Middleware/CorrelationIdMiddleware.cs
+++ HomeLabCore.Api/Middleware/CorrelationIdMiddleware.cs
@@ -1,28 +1,45 @@
 ﻿using HomeLabCore.Api.Constants;
+using HomeLabCore.Api.Logging;
 using HomeLabCore.Shared.Constants;
 using HomeLabCore.Shared.Contexts;
 using Serilog.Context;
 
 namespace HomeLabCore.Api.Middleware;
 
-public class CorrelationIdMiddleware : IMiddleware
+public class CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
 {
-    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    public async Task InvokeAsync(HttpContext context)
     {
         context.Request.Headers.TryGetValue(HeaderNames.CorrelationId, out var receivedCorrelationIds);
 
-        var correlationId = receivedCorrelationIds.FirstOrDefault();
+        var receivedCorrelationId = receivedCorrelationIds.FirstOrDefault();
 
-        if (string.IsNullOrWhiteSpace(correlationId) || !Guid.TryParse(correlationId, out _))
+        string correlationId;
+
+        if (string.IsNullOrWhiteSpace(receivedCorrelationId) || !Guid.TryParse(receivedCorrelationId, out _))
         {
-            // TODO log here
-
             correlationId = CorrelationContext.CorrelationId;
+
+            using(LogContext.PushProperty(LogPropertyNames.CorrelationId, receivedCorrelationId))
+            {
+                logger.GeneratedNewRequestCorrelationId(receivedCorrelationId, correlationId);
+            }
         }
         else
         {
+            correlationId = receivedCorrelationId;
             CorrelationContext.CorrelationId = correlationId;
         }
+
+        context.Response.OnStarting(() =>
+        {
+            if (!context.Response.Headers.ContainsKey(HeaderNames.CorrelationId))
+            {
+                context.Response.Headers.Append(HeaderNames.CorrelationId, correlationId);
+            }
+
+            return Task.CompletedTask;
+        });
 
         using (LogContext.PushProperty(LogPropertyNames.CorrelationId, correlationId))
         {
